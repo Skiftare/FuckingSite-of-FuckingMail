bde9ceaf98eb574f47fa3e8445a0d39d95fc061a
Jakob Ornysh <jornysh@gmail.com>
2022-12-20 09:56:49 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function button() {
 }
 
 function addNewFile(name) {
-    var ul = document.getElementById("list");
-    var li = document.createElement("li");
+    const ul = document.getElementById("list");
+    const li = document.createElement("li");
     li.innerHTML = name;
     ul.appendChild(li);
 }
@@ -23,8 +23,8 @@ list = document.getElementById('list');
 fileUploader.onchange = function() {
     let files = event.target.files;
     list.style.display = 'block';
-    
-    del = document.getElementById('empty-list');
+
+    let del = document.getElementById('empty-list');
     del.style.display = 'none';
     for (let i = 0; i < files.length; i++) {
         console.log(files[i].name);
@@ -32,8 +32,28 @@ fileUploader.onchange = function() {
     }
 }
 
-theme = document.getElementById('theme')
+theme = document.getElementById('theme');
+activated = false;
 
 theme.onclick = function () {
     console.log('Changed theme');
+    activated = !activated;
+    console.log(activated);
+
+    let body = document.querySelector('body');
+    // let input = document.querySelector('.input');
+    // let p = document.querySelector('p');
+    // let list = document.querySelector('.list');
+
+    if (activated) {
+        body.style.background = "linear-gradient(45deg, #4B155C, #471578) no-repeat center center fixed";
+        // input.style.background = "#1C53D6";
+        // p.style.background = "#1C53D6";
+        // list.style.background = "#1C53D6";
+    } else {
+        body.style.background = "linear-gradient(45deg, rebeccapurple, lightcoral) no-repeat center center fixed";
+        // input.style.background = "#456BD9";
+        // p.style.background = "#456BD9";
+        // list.style.background = "#456BD9";
+    }
 }
